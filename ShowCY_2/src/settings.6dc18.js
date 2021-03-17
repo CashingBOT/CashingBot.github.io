5abe7d3efefa5e56652529974fdc6b6f64035ed8
ResolCaT <908078607@qq.com>
2021-03-18 02:10:06 +0800

    Add<ShowCY_2>

---
+++ ShowCY_2/src/settings.6dc18.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Page.fire",orientation:"",jsList:[],bundleVers:{internal:"69b9a",resources:"ab621",main:"c1038"}};
